8f165e78f5f6950b15d91476956efc598dc4f077
mike suarez <mikemps@gmail.com>
2013-09-23 22:30:38 -0400

    showing color based on status

--- sample/status.js
+++ sample/status.js
@@ -1,3 +1,10 @@
-function Servers($scope) {
-  $scope.servers = servers;
-}
+angular
+  .module('status', [])
+  .controller('Servers', function ($scope) {
+    $scope.servers = servers;
+  })
+  .filter('status_style', function(){
+    return function(is_done) {
+      return is_done ? 'success' : 'default'
+    }
+  });
